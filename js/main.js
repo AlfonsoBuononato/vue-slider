f92211d544db699a3b9422cef73a9dbd593b8fa4
Alfonso Buononato <74600557+AlfonsoBuononato@users.noreply.github.com>
2021-04-21 16:25:04 +0200

    esercizio

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        image:[
+            "img/img.jpg",
+            "img/img2.jpg",
+            "img/img3.jpg",
+            "img/img4.jpg",
+        ],
+        indexImage: 0,
+    },
+    methods: {
+        prevImage() {
+            this.indexImage --
+            if(this.indexImage < 0){
+                this.indexImage = this.image.length -1;
+            }
+        },
+        nextImage() {
+            this.indexImage ++
+            if(this.indexImage > (this.image.length -1)){
+                this.indexImage = 0;
+            }
+        },
+        setImage(index) {
+            this.indexImage === index;
+            console.log(index);
+        } 
+    }
+})
